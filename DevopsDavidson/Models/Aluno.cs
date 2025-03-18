e937abf6a877dedc678ef236966a81a73ee28eee
D4avidson <davidsonsampaio08@gmail.com>
2025-03-18 09:07:05 -0300

    classe Aluno

---
+++ DevopsDavidson/Models/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace DevopsDavidson.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public String ?Nome { get; set; }
+        public String ?Rgm { get; set; }
+        public String ?Professor { get; set; }
+        public String ?Email { get; set; }
+        public DateTime Idade { get; set; }
+      
+    }
+}
